2248557b43269eae1db43127096d4ae6c7a87d68
Evan Chen <evan@evanchen.cc>
2020-07-23 23:00:24 -0700

    Better sync script

--- sync.sh
+++ sync.sh
@@ -6,3 +6,4 @@ gsutil -m rsync -x ".gitignore|sync.sh|Makefile" . gs://web.evanchen.cc/guessr/
 gsutil -m rsync css/ gs://web.evanchen.cc/guessr/css
 gsutil -m rsync js/ gs://web.evanchen.cc/guessr/js
 gsutil -m rsync diagrams/ gs://web.evanchen.cc/guessr/diagrams
+gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/guessr/*
