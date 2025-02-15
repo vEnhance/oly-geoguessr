9c780735a98d7965259b862a5b9c81493114422d
Evan Chen <evan@evanchen.cc>
2025-02-15 12:44:49 -0500

    fix: sync.sh cache patch should be **

--- sync.sh
+++ sync.sh
@@ -6,4 +6,4 @@ gcloud storage rsync -x ".gitignore|sync.sh|Makefile" . gs://web.evanchen.cc/gue
 gcloud storage rsync css/ gs://web.evanchen.cc/guessr/css
 gcloud storage rsync js/ gs://web.evanchen.cc/guessr/js
 gcloud storage rsync diagrams/ gs://web.evanchen.cc/guessr/diagrams
-gcloud storage objects update --cache-control="private, max-age=0" gs://web.evanchen.cc/guessr/*
+gcloud storage objects update --cache-control="private, max-age=0" "gs://web.evanchen.cc/guessr/**"
